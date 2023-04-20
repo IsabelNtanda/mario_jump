13f9de71873427a12935a46090397665d8cefec1
Isabel Pereira <isabelalvaro116@gmail.com>
2023-04-20 12:02:47 -0300

    added audio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,54 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
-const game_over = document.querySelector('.game-over, .hidden')
-const jump = () => { 
-    mario.classList.add('jump')
+const game_over = document.querySelector('#game_over_img')
+const btn = document.querySelector('#button')
+const jump_song = document.querySelector('.jump_song')
+const over_song = document.querySelector('.game_over_song')
 
-    // setTimeout(funcao, acao) 
-    setTimeout( () => {
+const jump = () => {
+  mario.classList.add('jump')
+
+  jump_song.currentTime = 0.1
+  jump_song.volume = 0.1
+  jump_song.play()
+
+  setTimeout(() => {
     mario.classList.remove('jump')
-    },500)
-
+  }, 500)
 }
-const loop = setInterval(() =>{
 
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-const pipePosition = pipe.offsetLeft
-// + -> converte para numero
-const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
-
-
-if (pipePosition < 12 && pipePosition > 0  && marioPosition < 8 ){
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
     pipe.style.animation = 'none'
-    pipe.style.left = `${pipePosition}rem`
+    pipe.style.left = `${pipePosition}px`
 
     mario.style.animation = 'none'
-    mario.style.bottom = `${ marioPosition}rem`
+    mario.style.bottom = `${marioPosition}px`
 
     mario.src = './assets/images/game-over.png'
-    mario.style.width = '9rem'
-    mario.style.margin = '5rem'
+    mario.style.width = '75px'
+    mario.style.marginLeft = '50px'
 
-    game_over.classList.add('game-over')
+    game_over.classList.add('.game_over')
     game_over.classList.remove('hidden')
 
-    clearInterval(loop)
-}
+    btn.classList.add('.game_over')
+    btn.classList.remove('hidden')
 
-} , 10)
+    btn.addEventListener('click', function () {
+      location.reload()
+    })
+
+    over_song.currentTime = 0.1
+    over_song.volume = 0.2
+    over_song.play()
+
+    clearInterval(loop)
+  }
+}, 10)
+
 document.addEventListener('keydown', jump)
 document.addEventListener('click', jump)
